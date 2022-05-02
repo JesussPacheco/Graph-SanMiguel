f9f0a47ab8c5b4df20697926d39d96a90a02c3a1
Jesus Pacheco <jesu102012@gmail.com>
2022-05-02 17:34:37 -0500

    advance

--- solution.py
+++ solution.py
@@ -7,10 +7,16 @@ with open("ways.txt") as f_obj:
         k,v_text=line.strip().split('.')
         v=tex_to_list(v_text)
         ways[k.strip()]=v
-#print(ways['27455207'])  
-    #for calle in ways:
-     #   print(calle) 
+ #diccionario de nodos       
+new_id={}
+with open("nodes.txt") as f_obj:  
+    index=0
+    for line in f_obj:
+        new_id[line]=index
+        index+=1
+
 prueba={1:[123,41234],2:[8612,145,444],8:[999,88,33]}
-for i in ways:
-    print(f"{i} : {ways[i]}")
+
+for prueba in ways:
+    print(ways[prueba])
      
